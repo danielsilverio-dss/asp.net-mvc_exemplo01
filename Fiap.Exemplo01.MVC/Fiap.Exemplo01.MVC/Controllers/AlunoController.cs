833da5a5f213f388380e27216acbfa3cd7e3ecc6
logonrm <logonrm@fiap.com.br>
2017-08-14 20:48:24 -0300

    first commit

---
+++ Fiap.Exemplo01.MVC/Fiap.Exemplo01.MVC/Controllers/AlunoController.cs
@@ -0,0 +1,29 @@
+﻿using Fiap.Exemplo01.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo01.MVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Cadastro(Aluno aluno)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Aluno aluno)
+        {
+            ViewBag.nomeAluno = aluno.Nome;
+            ViewBag.dataAluno = aluno.Data;
+            ViewBag.salarioAluno = aluno.Salario;
+            TempData["mensagem"] = "Aluno Registrado!";
+            return View(aluno);
+        }
+    }
+}
---
+++ Fiap.Exemplo01.MVC/Fiap.Exemplo01.MVC/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo01.MVC.Models
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public DateTime Data { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
